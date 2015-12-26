50c20368a12fa57b7e4ce4a1383a2e2b8e1c4f72
Floatas <floatas@outlook.com>
2015-12-26 16:54:18 +0200

    Service logic

--- ArduinoService/ArduinoService.cs
+++ ArduinoService/ArduinoService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO.Ports;
 using System.Linq;
 using System.Reflection;
 using System.ServiceProcess;
@@ -13,6 +14,8 @@ namespace ArduinoService
         private SensorDataRepository _sensorData;
         private SensorData _lastData;
         private Logger _logger;
+        private System.Timers.Timer _timer;
+        const string GetHeight = "1";
 
         public ArduinoService()
         {
@@ -27,10 +30,49 @@ namespace ArduinoService
             _lastData = _sensorData.LastEntrie();
 
             _logger.Info(GetPropertyValues(_lastData) ?? "No last data found");
+
+            serialPort1.PortName = "COM3";
+            serialPort1.BaudRate = 9600;
+            serialPort1.DataReceived += _serialPort_DataReceived;
+
+            _timer = new System.Timers.Timer();
+            _timer.Interval = 5000;
+            _timer.Elapsed += _timer_Elapsed;
+
+            _timer.Start();
+        }
+
+        private void _timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            MonitorHeight();
+        }
+
+        private void MonitorHeight()
+        {
+            try
+            {
+                if (serialPort1.IsOpen == false)
+                {
+                    serialPort1.Open();
+                }
+
+                serialPort1.Write(GetHeight);
+            }
+            catch (Exception e)
+            {
+                _logger.Error(e);
+            }
+        }
+
+        private void _serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            var data = new SensorData { DateTime = DateTime.UtcNow, Height = Int32.Parse(serialPort1.ReadLine()) };
+            _sensorData.Add(data);
         }
 
         protected override void OnStop()
         {
+            _timer.Stop();
             _logger.Info("Service stopped");
         }
 
